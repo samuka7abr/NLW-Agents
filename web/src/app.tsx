94c7e9c501f2a5162aa2d88f768d251af5caee2f
samuka7abr <samuelabrao2006@gmail.com>
2025-07-09 16:27:14 -0300

    Front-end: rooms display

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,19 @@
-export function App(){
-    return(
-        <div className="bg-amber-300">
-        <h1>Hello World!</h1>
-        </div>
-    );
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
+
+const queryClient = new QueryClient()
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
 }
--- web/src/lib/utils.tsx
+++ web/src/lib/utils.tsx
@@ -1,5 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
+// biome-ignore assist/source/organizeImports: <no need>
 import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
